2f1a8fa6fad5ece78c59ab3306470957f02b4d9a
Gaurav Dangwal <gdangwal268@gmail.com>
2024-11-12 21:06:20 +0530

    New update

--- src/app.js
+++ src/app.js
@@ -4,14 +4,83 @@ const app = express();
 const User = require("./models/user");
 
 
-app.use(express.json());
+app.use(express.json());   //MiddleWare
+
+//GET user by Email
+
+app.get("/user", async(req,res)=>{
+    const userEmail = req.body.emailId;
+
+    try{
+    const users = await User.find({emailId: userEmail});
+    res.send(users);
+    }
+    catch(err){
+        res.status(404).send("Not found");
+    }
+});
+
+//Feed API. Feed/get(To get details of all the users).
+
+app.get("/feed",async (req,res)=>{
+
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }
+    catch(err){
+        res.status(404).send("Not able to respond");
+    }
+});
+
+//POST Api (user details to database)
 
 app.post("/signup",async(req,res)=>{
     
+    try{
     const user = new User(req.body);
     await user.save();
     res.send("data sent succesfully");
+    }
+    catch(err){
+        if (err.code === 11000){
+            res.status(404).send("Email already Registered");
+        } 
+        else{
+            res.status(500).send("*check your credentials*");
+        }
+    }
 });
+
+//Delete Api
+
+app.delete("/user",async (req,res)=>{
+    const userId = req.body.userId;
+    try{
+        const user = await User.findByIdAndDelete({ _id: userId});
+        res.send(user);
+    }
+    catch(err){
+        res.status(404).send("Not found");
+    }
+});
+
+//Update
+app.patch("/user",async (req,res)=>{
+    const userId = req.body.userId;
+    const data = req.body;
+    try{
+    await User.findByIdAndUpdate({ _id: userId },data,{
+        returnDocument:"after",
+        runValidators:true,
+    });
+    res.send("data Updated successfully");
+    }
+    catch(err){
+        res.status(404).send("Update Failed" + err.message) ;  
+    }
+});
+
 
 connectDB()
 .then(()=>{
--- src/models/user.js
+++ src/models/user.js
@@ -3,22 +3,41 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
     firstName :{
         type: String,
+        required: true,
+        minLength:4,
+        maxLength:20,
     },
     lastName :{
         type: String,
     },
     emailId:{
         type: String,
+        unique: true,
+        required:true,
+        lowercase:true,
     },
     password:{
         type: String,
+        required: true,
     },
     age:{
         type: Number,
+        min:18,
     },
     gender:{
         type: String,
+        validate(value){
+            if(!['Male','Female','Others'].includes(value)){
+                throw new Error("Gender data is not valid");
+            }
+        }
     },
+    about:{
+        type:String,
+        default:"This is the about section",
+    }
+},{
+    timestamps:true,
 })
 
 module.exports = mongoose.model("User",userSchema); 
